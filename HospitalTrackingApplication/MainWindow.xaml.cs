c442cdd60edcbfbe8576c3f6b2a6ad2bc7b14740
Pelimenya <portalplay@mail.ru>
2024-03-12 20:47:07 +0300

    add DeleteEllipse

---
+++ HospitalTrackingApplication/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using HospitalTrackingApplication.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace HospitalTrackingApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer timer = new DispatcherTimer();
+        private PersonLocations personLocations;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Get();
+            AddEllipsesBasedOnNumber();
+        }
+
+        private async void Timer_Tick(object? sender, EventArgs e)
+        {
+            await Get();
+            AddEllipsesBasedOnNumber();
+        }
+
+        private async Task Get()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync("http://localhost:8080/PersonLocations");
+                    response.EnsureSuccessStatusCode();
+                    string result = await response.Content.ReadAsStringAsync();
+                    personLocations = JsonConvert.DeserializeObject<PersonLocations>(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddEllipse(StackPanel stackPanel, SolidColorBrush brush, string name)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 10;
+            ellipse.Height = 10;
+            ellipse.Fill = brush;
+            stackPanel.Children.Add(ellipse);
+            ellipse.Name = name;
+        }
+
+        private void AddEllipsesBasedOnNumber()
+        {
+            try
+            {
+                foreach (var response in personLocations.response)
+                {
+                    SolidColorBrush brush = response.EliceColor;
+                    string stackPanelName = "ckud" + response.lastSecurityPointNumber;
+                    StackPanel stackPanel = (StackPanel)this.FindName(stackPanelName);
+                    string name = "elipce" + response.personCode.ToString();
+                    if (stackPanel != null && response.lastSecurityPointDirection == "in")
+                    {
+                        AddEllipse(stackPanel, brush, name);
+                    }
+                    if (stackPanel != null && response.lastSecurityPointDirection == "out")
+                    {
+                        DeleteEllipse(stackPanel, name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteEllipse(StackPanel stackPanel, string name)
+        {
+            UIElement elementToRemove = (UIElement)stackPanel.FindName(name);
+            if (elementToRemove != null)
+            {
+                stackPanel.Children.Remove(elementToRemove);
+            }
+        }
+    }
+}
---
+++ HospitalTrackingApplication/Models/PersonLocations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace HospitalTrackingApplication.Models
+{
+    class PersonLocations
+    {
+        public Response[] response { get; set; }
+    }
+
+    public class Response
+    {
+        public int personCode { get; set; }
+        public string personRole { get; set; }
+        public int lastSecurityPointNumber { get; set; }
+        public string lastSecurityPointDirection { get; set; }
+        public string lastSecurityPointTime { get; set; }
+
+        public SolidColorBrush EliceColor
+        {
+            get
+            {
+                var brush = Brushes.Transparent;
+
+                if (personRole == "Клиент" && lastSecurityPointDirection == "in")
+                {
+                    brush = Brushes.Green;
+                }
+                else if (personRole == "Сотрудник" && lastSecurityPointDirection == "in")
+                {
+                    brush = Brushes.Blue;
+                }
+                return brush;
+            }
+        }
+    }
+}
